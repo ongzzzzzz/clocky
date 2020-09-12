50e3655ed060be6a3c133c061ebf781dbb0dff85
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-09-12 19:00:04 +0800

    wEb dEsSiGN 👁‍🗨👄👁‍🗨

--- web/functions/index.js
+++ web/functions/index.js
@@ -24,8 +24,8 @@ function checkStatus(res) {
         throw new Error(res.statusText);
     }
 }
-
-exports.updateData = functions.pubsub.schedule('every 30 minutes').onRun((context) => {
+//exports.updateData = functions.pubsub.schedule('every 30 minutes').onRun((context) => {
+exports.updateData = functions.pubsub.schedule('every 24 hours').onRun((context) => {
     fetch('http://dataservice.accuweather.com/currentconditions/v1/234975?apikey=HuptOIEbq28g99YeSniD2juuFb8a2Btj')
         .then(checkStatus)
         .then(json => {
